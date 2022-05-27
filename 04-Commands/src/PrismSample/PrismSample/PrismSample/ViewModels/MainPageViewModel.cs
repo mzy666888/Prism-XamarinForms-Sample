b04611eaabd15bcaddaa0b3e84e6e9f39bd8fdd9
mzy_666888 <mzy_666888@126.com>
2022-05-27 18:53:11 +0800

    04 - 命令

---
+++ 04-Commands/src/PrismSample/PrismSample/PrismSample/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,43 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace PrismSample.ViewModels
+{
+    public class MainPageViewModel : ViewModelBase
+    {
+        public MainPageViewModel(INavigationService navigationService)
+            : base(navigationService)
+        {
+            Title = "Main Page";
+        }
+
+        private bool _isActive;
+
+        public bool IsActive
+        {
+            get => _isActive;
+            set => SetProperty(ref _isActive, value);
+        }
+
+        private string _text;
+
+        public string Text
+        {
+            get => _text;
+            set => SetProperty(ref _text, value);
+        }
+
+        private DelegateCommand<string> _buttonClickCommand;
+
+        public DelegateCommand<string> ButtonClickCommand =>
+            _buttonClickCommand ??= new DelegateCommand<string>((parameter) =>
+            {
+                Text = parameter;
+            }).ObservesCanExecute(() => IsActive);
+    }
+}
